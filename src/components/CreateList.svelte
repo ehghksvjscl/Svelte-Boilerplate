f231822e59b66e78cc24e0ee879876de222c7a41
ehghksvjscl <ehghkswodnjs@naver.com>
2022-04-24 12:24:13 +0900

    List 생성을 위한 수정 모드(Edit mode)

--- src/components/CreateList.svelte
+++ src/components/CreateList.svelte
@@ -1,12 +1,56 @@
 <script>
     import { lists } from '~/store/store'
+    import { tick } from 'svelte'
 
-    lists.add({
-        title: "Saemi!!"
-    })    
+    let isEditMode = false
+    let title = ""
+    let textEditEl
+
+    function addList() {
+        if (title.trim()) {
+            lists.add({
+                title
+            })
+        }
+        offEditMode()
+    }
+
+    async function onEditMode() {
+        isEditMode = true
+        await tick()
+        textEditEl && textEditEl.focus()
+    }
+
+    function offEditMode() {
+        isEditMode = false
+        title = ""
+    }
+
 </script>
 <div class="create-list">
-    + Add anoher list
+    {#if isEditMode}
+        <div class="edit-mode">
+            <textarea 
+                bind:value={title}
+                bind:this={textEditEl}
+                on:keydown={e => {
+                    e.key === "Enter" && addList()
+                    e.key === "Escape" && offEditMode()
+                    e.key === "Esc" && offEditMode()
+                }}
+                placeholder="Enter a title for this card..."></textarea>
+            <div class="actions">
+                <div class="btn" on:click={addList}>Add List</div>
+                <div class="btn" on:click={offEditMode}>Cancel</div>
+            </div>
+        </div>
+    {:else}
+        <div 
+            class="add-another-list"
+            on:click={onEditMode}>
+            + Add another list
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
