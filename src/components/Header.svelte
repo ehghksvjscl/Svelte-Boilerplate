93e7c1cb7208a98ef04e2ead4bd2da8dcd961e85
ehghksvjscl <ehghkswodnjs@naver.com>
2022-04-28 13:12:14 +0900

    Logo Change

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,12 @@
 <header>
-  <img
-    src="/images/trello-logo.svg"
+  <!-- <img
+    src="/images/urello_logo.svg"
     alt="Trello"
-    class="logo" />
+    class="logo" /> -->
+
+    <div class="container">
+      <div class="neon">Urello </div>
+    </div>
 </header>
 
 <style lang="scss">
@@ -14,8 +18,42 @@
     justify-content: center;
     align-items: center;
     img.logo {
-      width: 80px;
-      opacity: .5;
+      width: 120px;
     }
   }
+
+  .container {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .neon {
+    color: #FB4264;
+    font-size: 24px;
+    line-height: 9vw;
+    text-shadow: 0 0 3vw #F40A35;
+    outline: 0;
+    user-select: none;
+  }
+
+  .neon {
+    animation: neon 1s ease infinite;
+    -moz-animation: neon 1s ease infinite;
+    -webkit-animation: neon 1s ease infinite;
+  }
+
+  @keyframes neon {
+    0%,
+    100% {
+      text-shadow: 0 0 1vw #FA1C16, 0 0 3vw #FA1C16, 0 0 10vw #FA1C16, 0 0 10vw #FA1C16, 0 0 .4vw #FED128, .5vw .5vw .1vw #806914;
+      color: #FED128;
+    }
+    50% {
+      text-shadow: 0 0 .5vw #800E0B, 0 0 1.5vw #800E0B, 0 0 5vw #800E0B, 0 0 5vw #800E0B, 0 0 .2vw #800E0B, .5vw .5vw .1vw #40340A;
+      color: #806914;
+    }
+  }
+
+
 </style>
